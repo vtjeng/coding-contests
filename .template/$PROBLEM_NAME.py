8dde510b913688aefac5ad39e72d54ddd3fd714d
Vincent Tjeng <vincent.tjeng@gmail.com>
2021-03-19 11:54:17 -0700

    Simplify Python template.

--- .template/$PROBLEM_NAME.py
+++ .template/$PROBLEM_NAME.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
-import sys
+
+def process(case):
+    pass
 
 
-def main(lines):
-    return ""
+## Comment out the appropriate option
+# Option A: Multiple lines per case
+num_cases = int(input())
+for i in range(num_cases):
+    num_case_lines = int(input())
+    case = []
+    for j in range(num_case_lines):
+        case.append(input())
+    print(f"Case #{i+1}: {process(case)}")
 
-
-if __name__ == "__main__":
-    lines = []
-    for line in sys.stdin:
-        lines.append(line)
-
-    print(main(lines))
-
+# Option B: Single line per case
+num_cases = int(input())
+for i in range(num_cases):
+    print(f"Case #{i+1}: {process(input())}")
